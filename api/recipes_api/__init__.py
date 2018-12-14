f0e45572591c21bd28af4ee5c0b6b0e343f10e18
Borys Lemeshko <lemeshko.borys@gmail.com>
2018-12-14 17:42:43 +0200

    Added verbose name to recipes_api config

---
+++ api/recipes_api/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'recipes_api.apps.RecipesApiConfig'
--- api/recipes_api/apps.py
+++ api/recipes_api/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class RecipesApiConfig(AppConfig):
     name = 'recipes_api'
+    verbose_name = 'Рецепты'
