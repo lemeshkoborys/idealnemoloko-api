407c257323db53e8e588c81aec4b0f3118e4c851
Borys Lemeshko <lemeshko.borys@gmail.com>
2018-12-18 20:36:02 +0200

    Change order

--- api/recipes_api/views.py
+++ api/recipes_api/views.py
@@ -4,5 +4,5 @@ from rest_framework import generics
 
 class RecipeListView(generics.ListAPIView):
 
-    queryset = Recipe.objects.order_by('-id')
+    queryset = Recipe.objects.order_by('id')
     serializer_class = RecipeSerializer
