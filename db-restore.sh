6ada4ddfb0fc3b4136bccff50e1038ae7efa3684
Borys Lemeshko <lemes@vps625207.ovh.net>
2018-12-18 19:24:36 +0100

    Migration files

--- db-restore.sh
+++ db-restore.sh
@@ -1,3 +1,3 @@
 docker-compose up -d
-cat dump_final.sql | docker exec -i idealnemoloko-api_db_1 psql -U db_role idealnemoloko_db
+cat dump_final.sql | docker exec -i idealnemoloko-api_db_1_ed51eda56122 psql -U db_role idealnemoloko_db
 docker-compose stop
